a75c502d5d2e61c540f317d3ac0b1ba3000c6b9a
ap9149 <69780179+ap9149@users.noreply.github.com>
2020-08-17 09:13:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+var n1 = {name: "Samsung Galaxy Note 20", price: "$999", image: "Images/1.jpg", Summary: "Introducing Galaxy Note20 5G: The Power to Work. The Power to Play. This isn't the time to slow down, this is the time to forge ahead and take the opportunities that come your way. You don’t need a smartphone. You need a power phone. One as beautiful as it is intelligent with a pen that is mightier, a battery that doesn't leave you hanging and is as well-connected as you. Galaxy Note20 5G takes power to the next level with cutting-edge technology, letting you master whatever you choose to do next. Pen precision meets PC power with S Pen and Samsung Notes. So responsive, it feels like you're using a real pen to jot notes and sketch ideas. Then convert into Microsoft Word or PowerPoint, and save and sync across all your Galaxy devices while on the go. Intelligent Battery and Super Fast Charge learn from how you work and play to optimize battery life and boost your battery in minutes. Get the best Galaxy mobile gaming in the palm of your hand with its smooth gameplay experience. HyperFast Processor transforms your working, gaming, and bingeing so you can multitask without slowing down. Operate from mobile to PC seamlessly with Link to Windows and share files with a simple drop and drag. Power on with Galaxy Note20 5G to take your mobile experience to the next level."};
+
+var n2 = {name: "Google - Pixel 3a with 64GB", price: "$399", image: "Images/2.jpg", Summary: "Get more done with Pixel 3A. It has an extraordinary camera with features like night sight, portrait mode, and HDR+. a battery that charges fast and lasts all day [7]. All the helpfulness of the Google assistant built in. And 3 years of security and os updates included [8]. it’s everything you love about Google – in a phone. Works with all major carriers."};
+
+var n3 = {name: "Motorola Moto G8 Power Lite 64GB", price: "$119.99", image: "Images/3.jpg", Summary: "Fuel your freedom moto g8 power lite gives you a powerful 5000 mAh battery, designed to last up to 3 days on just a single charge . You can stream music for 100 hours, or watch shows and movies for 19 hours. Plus get more than 35 days of standby time, and know you have power when you need it most. Go faster from the get-go Browse, watch, work, and game in an instant thanks to a 2. 3 GHz octa-core processor. And with 4GB of RAM, your phone responds instantly to every touch, tap, and swipe, so you get to the good stuff faster. Larger than life Enjoy ultrawide views on the stunning 6.5 Max Vision HD+ display. You get the big screen you’ve been craving in a design that still fits comfortably in one hand. Unlike traditional displays, Max Vision provides maximum viewing with a 20: 9 aspect ratio, immersing you in photos, movies, shows, games, and everything else. Space for everything Never give storage a second thought. moto g8 power lite gives you 64 GB of space for apps, music, movies, and photos. Easily add up to 256 GB of your favorite apps, music, movies, and photos using a dedicated microSD card slot."};
+
+var n4 = {name: "Seagate Portable 2TB External Hard Drive Portable HDD", price: "$62.49", image: "Images/4.jpg", Summary: "Fast transfer rates When connected to a USB 3.0 port, WD Elements portable storage delivers maximum performance for data transfers. Transfer a full-length HD movie in a fraction of the time it takes with USB 2.0. Performance may vary based on user's hardware and system configuration. USB 3.0 and USB 2.0 compatibility Works seamlessly with the latest USB 3.0 devices and is backward compatible with USB 2.0 devices as well. High capacity in a small enclosure The small, lightweight design offers high-capacity storage, making WD Elements portable storage the ideal companion for consumers who want to take their important files with them. Improve PC performance When your internal drive is almost full your PC slows down. Don't delete files. Free up space on your internal drive by transferring files to your WD Elements and get your PC moving again. WD quality inside and out We know your data is important to you. So we build the drive inside to our demanding requirements for durability, shock tolerance and long-term reliability. Then we protect the drive with a durable enclosure designed for style and protection. Formatted for Windows Formatted NTFS and compatible with Windows 10, 8.1 and 7. Reformatting required for other operating systems."};
+
+var n5 = {name: "AMD Ryzen 7 3700X", price: "$283.55", image: "Images/5.jpg", Summary: "AMD Ryzen 7 3700X 8 core, 16 thread unlocked desktop processor with Wraith Prism LED cooler. Base Clock - 3.6GHz.Default TDP / TDP: 65W."};
+
+var n6 = {name: "High-Speed HDMI Cable, 6 Feet", price: "$6.99", image: "Images/6.jpg", Summary: "Bridging the gap between audio and video in one dynamic cable, you can connect your Blu-ray player, computer, AV receiver, Apple TV, Roku, cable box, Play Station 3, Play Station, Xbox One, 360, Nintendo Wii, Wii U or other HDMI compatible devices to your Ultra 4K TV (UHD TV), HDTV, monitor, or projector. The AmazonBasics HDMI cable displays 4K video at 24 Hz, 1080p at 60 Hz, with 48-bit deep color, and supports bandwidth of 10.2 Gbps."};
+
+var n7 = {name: "Logitech C920 Hd Pro Webcam (Black)", price: "$164.99", image: "Images/7.jpg", Summary: "Full HD 1080p video that's faster, smoother and works on more computers.   Skype in Full HD 1080p Get breathtaking Full HD 1080p video calls on Skype for the sharpest video-calling experience.   Smoother. Sharper. Richer. Clearer. Logitech Fluid Crystal Technology. It's what makes a Logitech webcam better. It's smoother video, sharper pictures, richer colors and clearer sound in real-world conditions.   Call in HD 720p Enjoy widescreen HD 720p video on most major IMs and Logitech Vid HD.   HD your Facebook One-click HD upload (1080p) to Facebook, Twitter and YouTube makes it easy to share your life with friends and family."};
+
+var n8 = {name: "Fitbit Versa 2 Health and Fitness Smartwatch", price: "$198.95", image: "Images/8.jpg", Summary: "Meet Fitbit versa 2 a smartwatch that elevates every moment. Use your voice to create alarms, set bedtime reminders or check the weather with amazon Alexa built in. Take your look from the gym to the office with its modern and versatile design. See your stats with an always-on display mode. In addition, get Fitbit pay, daily sleep quality scores, apps, notifications, 24/7 heart rate and store 300+ songs for an experience that revolves around you."};
+
+var n9 = {name: "Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS", price: "$89.99", image: "Images/9.jpg", Summary: "The Acer SB220Q bi 21. 5 Widescreen LCD IPS display combines stylish ultra-thin functionality with amazing 1920 x 1080 resolution, allowing you to enjoy High-Definition entertainment in the comfort of your home. Incredible 100, 000, 000: 1 contrast ratio and rapid 4ms response time highlight the advanced technology, The SB220Q bi LCD also features multiple inputs (HDMI & VGA) for greater compatibility, a Kensington lock slot to protect your investment, and Energy Star compliance to help you save money and energy. Standby Power Consumption - 450 maw. (UM. WS0AA. 003). Height without stand - 11. 9 inches, Width - 19. 6 inches, Depth - 1. 4 inches. Weight (Approximate) - 4. 41 pounds. Weight with Stand (Approximate) - 5. 47 pounds."};
+
+var n10 = {name: "TP-Link 5 Port Gigabit Ethernet Network Switch (TL-SG105)", price: "$16.99", image: "Images/10.jpg", Summary: "TP-LINK’s TL-SG105 provides an easy way to expand your wired network. All 5 ports support Auto-MDI/MDIX, eliminating the need to worry about what type of cable to use. Featuring full duplex mode, the device can process data at a rate of up to 2000Mbps, making it an ideal choice for expanding your high performance wired network. Moreover, with innovative energy-efficient technology, the TL-SG105 can save energy up to 65%."};
+
+var checks = [];
+var carte = [];
+function getValue(){
+
+	checks = document.getElementsByClassName('checks');
+	for(i = 0; i < 10;i++)
+	{
+		if(checks[i].checked === true){
+			carte.push(checks[i].id);
+		}
+	}
+	localStorage.setItem('cart', carte);
+}
+
+function uncheckAll(){
+   $('input[type="checkbox"]:checked').prop('checked',false);
+}
+
+function loadSections() {
+	carte = localStorage.getItem('cart');
+	if(carte.length === 0)
+	{
+		alert("Cart Empty");
+	}
+	const sectionBody = document.getElementById('info');
+	let datahtml = '';
+	for(let i of carte){
+		console.log(i);
+		if(i == 1){
+			datahtml += '<section> <div class="container" > <img src="'+n1.image+'" height="300" width=auto><h3>'+n1.name+'</h3> <p>'+n1.Summary+'</p></div></section>'
+		}
+		if(i == 2){
+			datahtml += '<section> <div class="container" > <img src="'+n2.image+'" height="300" width=auto><h3>'+n2.name+'</h3> <p>'+n2.Summary+'</p></div></section>'
+		}
+		if(i == 3){
+			datahtml += '<section> <div class="container" > <img src="'+n3.image+'" height="300" width=auto><h3>'+n3.name+'</h3> <p>'+n3.Summary+'</p></div></section>'
+		}
+		if(i == 4){
+			datahtml += '<section> <div class="container" > <img src="'+n4.image+'" height="300" width=auto><h3>'+n4.name+'</h3> <p>'+n4.Summary+'</p></div></section>'
+		}
+		if(i == 5){
+			datahtml += '<section> <div class="container" > <img src="'+n5.image+'" height="300" width=auto><h3>'+n5.name+'</h3> <p>'+n5.Summary+'</p></div></section>'
+		}
+		if(i == 6){
+			datahtml += '<section> <div class="container" > <img src="'+n6.image+'" height="300" width=auto><h3>'+n6.name+'</h3> <p>'+n6.Summary+'</p></div></section>'
+		}
+		if(i == 7){
+			datahtml += '<section> <div class="container" > <img src="'+n7.image+'" height="300" width=auto><h3>'+n7.name+'</h3> <p>'+n7.Summary+'</p></div></section>'
+		}
+		if(i == 8){
+			datahtml += '<section> <div class="container" > <img src="'+n8.image+'" height="300" width=auto><h3>'+n8.name+'</h3> <p>'+n8.Summary+'</p></div></section>'
+		}
+		if(i == 9){
+			datahtml += '<section> <div class="container" > <img src="'+n9.image+'" height="300" width=auto><h3>'+n9.name+'</h3> <p>'+n9.Summary+'</p></div></section>'
+		}
+		if(i == 0){
+			datahtml += '<section> <div class="container" > <img src="'+n10.image+'" height="300" width=auto><h3>'+n10.name+'</h3> <p>'+n10.Summary+'</p></div></section>'
+		}
+	}
+
+	sectionBody.innerHTML = datahtml;
+
+}
